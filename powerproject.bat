f850f3d0c4650ada87dad1098c5c1608bbab38fd
Antimoon51 <50957040+Antimoon51@users.noreply.github.com>
2019-11-04 19:36:46 +0100

    Version 1.0
    First working version.

--- powerproject.bat
+++ powerproject.bat
@@ -9,6 +9,7 @@ pause
 cls
 setlocal EnableDelayedExpansion
 echo The following devices are able to wake your PC:
+:return
 SET count=1
 FOR /F "tokens=* USEBACKQ" %%F IN (`powercfg devicequery wake_armed`) DO (
   SET h=%%F
@@ -17,6 +18,33 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`powercfg devicequery wake_armed`) DO (
   SET /a count+=1
 )
 pause
+echo errorlevel: %errorlevel%
+echo Type the number of the device you like to disable.
+echo To abort type 'end'
+set /p string=Disable this Device:
+if "%string%"=="end" (
+	goto exit
+)
+echo Device to disable: %string%
+set count=1
 
+pause
+FOR /F "tokens=* USEBACKQ" %%N IN (`powercfg devicequery wake_armed`) DO (
+	set x=%%N
+	goto if
+	:back
+	set /a count+=1
+)
+goto return
+:if
+if "!count!"=="!string!" (
+	powercfg devicedisablewake "!x!"
+	goto back
+)
+pause
+:exit
+echo errorlevel: %errorlevel%
+pause
 
 Endlocal
+exit 
